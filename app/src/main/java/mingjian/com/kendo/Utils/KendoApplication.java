9386e3ea495e4f442b2efd5dd069f243e32d3e35
ryan.chen <ryan.chen@harman.com>
2016-11-02 17:38:32 +0800

    add leancloud SDK

--- app/src/main/java/mingjian/com/kendo/Utils/KendoApplication.java
+++ app/src/main/java/mingjian/com/kendo/Utils/KendoApplication.java
@@ -2,7 +2,10 @@ package mingjian.com.kendo.Utils;
 
 import android.app.Application;
 
+import com.avos.avoscloud.AVOSCloud;
 import com.facebook.drawee.backends.pipeline.Fresco;
+
+import mingjian.com.kendo.Common.Commons;
 
 /**
  * Created by studio02 on 10/25/16.
@@ -13,5 +16,7 @@ public class KendoApplication extends Application{
     public void onCreate() {
         super.onCreate();
         Fresco.initialize(this);
+        // 初始化参数依次为 this, AppId, AppKey
+        AVOSCloud.initialize(this, Commons.APP_ID,Commons.APP_KEY);
     }
 }
